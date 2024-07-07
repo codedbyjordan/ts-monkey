55eab3e2babfc3defe4046e1dc476fb1bb3d46a3
Jordan Baron <jordannbaron@gmail.com>
2024-07-07 02:11:18 -0400

    rm; readd later

--- src/utils/execute-js.ts
+++ src/utils/execute-js.ts
@@ -1,20 +1,12 @@
 import logger from "./logger";
-import { writeToOutput } from "./write-to-output";
 
 export default function executeJs(jsCode: string) {
   const originalLog = console.log;
   console.log = logger.log;
 
-  const last = jsCode.trim().split("\n").pop();
+  const fn = new Function(jsCode);
 
-  const fn = new Function(`
-    ${jsCode}
-    return ${last};
-  `);
-
-  const result = fn();
-
-  if (result) writeToOutput(null, result);
+  fn();
 
   console.log = originalLog;
 }
